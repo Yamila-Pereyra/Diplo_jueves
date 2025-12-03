166b4148658cc82367035a2852a9a0f0c3096e87
Yamila-Pereyra <yamilasoledadpereyra@hotmail.com>
2025-12-03 23:40:53 +0100

    M3U3

---
+++ M3U3/e1.js
@@ -0,0 +1,8 @@
+function promedioHistoria(nota1, nota2) {
+    let promedio = (nota1 + nota2) / 2;
+    return promedio;
+}
+
+// Ejemplo de uso:
+let resultado = promedioHistoria(6, 8);
+console.log(`El promedio de Historia es: ${resultado}`);
