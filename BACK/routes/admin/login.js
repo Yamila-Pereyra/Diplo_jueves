f9916100008f94136d549f2adbdbd9627971ff06
Yamila-Pereyra <yamilasoledadpereyra@hotmail.com>
2026-02-02 19:21:30 +0100

    BACK

--- BACK/routes/admin/login.js
+++ BACK/routes/admin/login.js
@@ -1,11 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var usuarioModel = require('./../../models/usuariosModel');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/login', { //login.hbs 
+/* GET login */
+router.get('/', function (req, res, next) {
+  res.render('admin/login', {
     layout: 'admin/layout'
-   });
+  });
+});
+
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+
+/* POST login */
+router.post('/', async (req, res, next) => {
+  try {
+    const usuario = req.body.usuario;
+    const password = req.body.password;
+
+    const data = await usuarioModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.id_nombre = data.usuario;
+
+
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.render('admin/login', {
+      layout: 'admin/layout',
+      error: true
+    });
+  }
 });
 
 module.exports = router;
