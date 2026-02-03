538c44461b913cd13fa878d0763907f911ab7704
Yamila-Pereyra <yamilasoledadpereyra@hotmail.com>
2026-02-04 00:17:31 +0100

    BACK

---
+++ BACK/database.sql
@@ -0,0 +1,30 @@
+INSERT INTO proyecto.novedades (titulo,subtitulo,cuerpo) VALUES
+	 ('Tendencias diseño web 2026: claves para adelantarte al futuro','Conoce las tendencias de diseño web para 2026: inspiración, guía y claves para adelantarte al futuro y entender la nueva estética digital.','El diseño web está en constante evolución, y cada año surgen nuevas tendencias que transforman la manera en que los usuarios interactúan con los sitios. En 2025, algunas de estas tendencias no solo mejorarán la estética y funcionalidad, sino que también influirán profundamente en la experiencia del usuario.
+
+En este artículo, exploramos las principales tendencias que marcarán el futuro del diseño web, que pueden mejorar la efectividad de cualquier página en un mundo digital cada vez más competitivo.'),
+	 ('10 elementos de una página web de éxito','Elementos de una página web de éxito','1. Navegación por tu página web
+La navegación es uno de los elementos de un sitio web que más cuidados deben estar. Una navegación intuitiva y fácil de usar permite a los visitantes explorar tu sitio sin esfuerzo, encontrando rápidamente la información que necesitan y pudiendo comprar las soluciones que necesitan en tan solo unos clics.
+
+Plantear una estructura clara y organizada facilita la navegación, evitando la confusión y la frustración del usuario y, por tanto, mejorando su experiencia. Como consecuencia de esto, el tiempo de permanencia en el sitio tiende a aumentar y se reduce la tasa de rebote, dos aspectos clave a la hora de conseguir una mejor tasa de conversión.
+
+2. Paleta de colores de la página web
+Puede que no sea de las primeras cosas en las que pienses cuando te preguntas cuáles son los elementos de un sitio web de éxito, pero elegir una paleta de colores apropiada es crucial.
+
+Los colores transmiten emociones y establecen una conexión visual con los visitantes, por lo que una paleta bien seleccionada crea una atmósfera coherente y atractiva que además fortalece la identidad de marca. Los colores adecuados también tienen el poder de guiar la atención del usuario hacia elementos importantes, así como de mejorar la legibilidad del contenido.
+
+Una combinación equilibrada de colores creará una experiencia agradable y memorable, dejando además una impresión duradera en los visitantes. Por lo general, tendrás un color primario que cubrirá la mayoría de tu página web, un color secundario para crear contrastes llamativos y un tercer color para los matices.
+
+3. Imágenes destacadas
+Las imágenes destacadas desempeñan un papel fundamental en el éxito de una página web porque capturan la atención de los visitantes y transmiten información de manera visual y atractiva. 
+
+Es importante contar con imágenes de calidad que sean relevantes, ya que refuerzan la identidad de marca y generan confianza en los usuarios. Además, las imágenes pueden mejorar la legibilidad y la comprensión del contenido, especialmente cuando se utilizan de manera estratégica para ilustrar conceptos clave.
+
+Puedes empezar con fotografías de bancos de imágenes, pero lo más apropiado para conseguir una web de éxito es sustituirlas por imágenes propias que personalicen mucho más la experiencia. 
+
+4. Presencia en las redes sociales
+No todos los elementos importantes en el diseño de sitios web exitosos están dentro de la página. Uno de ellos, por ejemplo, es el tráfico social, el que se genera desde redes sociales con una presencia estable y bien trabajada.
+
+Compartir contenido de tu página web en las redes sociales aumenta la visibilidad y el tráfico hacia tu sitio. Las redes sociales permiten la viralización de tu contenido, lo que puede generar más visitas y convertir tu sitio en una web de éxito.
+
+Además, integrar enlaces a tus perfiles sociales en tu página web permite a los visitantes conectarse y seguirte en plataformas populares, lo que amplía tu alcance y te brinda una forma de interactuar y mantener una comunidad activa. Aprovecha el poder de las redes sociales para impulsar el éxito de tu página web.'),
+	 ('prueba11-','prueba11','prueba11');
