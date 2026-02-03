6e6db53c67040b0b55fe9930810f9e61be5bc145
Yamila-Pereyra <yamilasoledadpereyra@hotmail.com>
2026-02-03 14:54:21 +0100

    BACK

--- BACK/routes/admin/novedades.js
+++ BACK/routes/admin/novedades.js
@@ -1,12 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-/* GET login */
 router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
     });
+});
+
+/* diseño */
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (
+            req.body.titulo != "" &&
+            req.body.subtitulo != "" &&
+            req.body.cuerpo != ""
+        ) {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargó la novedad'
+        });
+    }
 });
 
 module.exports = router;
