0b7a697a6c8361958214b1b625c4a449efbf9cd2
Yamila-Pereyra <yamilasoledadpereyra@hotmail.com>
2026-02-03 23:53:20 +0100

    BACK

--- BACK/models/novedadesModel.js
+++ BACK/models/novedadesModel.js
@@ -10,7 +10,7 @@ async function getNovedades() {
 async function insertNovedad(obj) {
     try {
         var query = "insert into novedades set ? ";
-        var rows = await pool.query(query, [obj])
+        var rows = await pool.query(query, [obj]);
         return rows;
 
     } catch (error) {
@@ -19,4 +19,31 @@ async function insertNovedad(obj) {
     }
 }
 
-module.exports = { getNovedades, insertNovedad }
+
+async function deleteNovedadesById(id) {
+    
+        var query = "delete from novedades where id = ? ";
+        var rows = await pool.query(query, [id]);
+        return rows;
+
+    }
+
+async function getNovedadById(id) {
+    
+        var query = "select * from novedades where id = ? ";
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+
+    }
+    async function modificarNovedadById(obj, id) {
+        try {
+            var query = 'update novedades set ? where id=?';
+            var rows = await pool.query(query, [obj,id]);
+            return rows;
+        } catch (error) {
+            throw error;
+        }
+        
+    }
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadesById, getNovedadById, modificarNovedadById}
